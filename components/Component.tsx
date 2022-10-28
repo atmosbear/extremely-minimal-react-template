1c61e708e301e89d34fd56b02bd031290f061187
atmosbear <mondegreen84@protonmail.com>
2022-10-28 06:16:52 -0700

    Added branch reminder to component.

--- components/Component.tsx
+++ components/Component.tsx
@@ -2,6 +2,7 @@ import React from "react"
 
 export default function Component() {
 	return (
-		<div >This is a component.</div>
+		<div >This is the minimal react branch with no extra bloat. 
+			Don't forget to switch to the correct branch if you want something else!</div>
 	)
 }
